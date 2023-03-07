1321e41d9243539c83f5a49c11528b7a7314b10e
eacorsab <emascord@espol.edu.ec>
2023-03-07 07:20:52 -0500

    obtener por id

--- api-Backend/app/productos/rutas/obtener_productos.py
+++ api-Backend/app/productos/rutas/obtener_productos.py
@@ -1,3 +1,4 @@
+from urllib import request
 from flask import jsonify
 from flask_cors import cross_origin
 from app.productos import bp
@@ -36,3 +37,36 @@ def obtener_productos():
     data = [row._asdict() for row in query_result]
     
     return jsonify(data)
+
+@bp.route('/obtener_productos/<codigo_Prod>', methods=['GET'])
+@cross_origin()
+def obtener_productos_por_codigo(codigo_Prod):
+    print(codigo_Prod)
+    
+    query_result = db.session.query(
+    Producto.artcodigo.label('codigo'),
+    Producto.artdescri.label('descripcion'),
+    Producto.artprecventa1.label('precio'),
+    Medida.meddescri.label('medida'),
+    Marca.mardescri.label('marca'),
+    Presentacion.predescri.label('presentacion'),
+    Linea.lindescri.label('linea'),
+    Producto.tmpcantidadimpresion.label('cantidad_etiquetas'),
+).join(Marca, Producto.marcodigo == Marca.marcodigo)\
+.join(Medida, Producto.medcodigo == Medida.medcodigo)\
+.join(Presentacion, Producto.precodigo == Presentacion.precodigo)\
+.join(Linea, Producto.lincodigo == Linea.lincodigo)\
+.filter(
+    
+    Producto.artcodigo == str(codigo_Prod)
+).order_by(Producto.artcodigo).all()
+
+# El resultado de la consulta es un objeto Row, que no puede ser serializado en JSON directamente.
+# Para convertir los objetos Row en diccionarios se utiliza el método as_dict() que se puede agregar en el modelo de SQLAlchemy. 
+# Luego, se puede construir una lista de diccionarios y devolverla como una respuesta JSON
+    data = [row._asdict() for row in query_result]
+    
+    return jsonify(data)
+
+    
+
--- api-Backend/myvenv/Scripts/activate.bat
+++ api-Backend/myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\DOCUMENTOS\DIGOTEC\projects\WorkSpace\Veltrix_React_v4.2.0\Admin\api-Backend\virtualenv
+set VIRTUAL_ENV=C:\Users\emily\OneDrive\Escritorio\ESPOL\2022-II\empresariales\proyectoFuture\SiacPracticasa_Proyecto\api-Backend\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(virtualenv) %PROMPT%
+set PROMPT=(myvenv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(virtualenv) 
+set VIRTUAL_ENV_PROMPT=(myvenv) 
 
 :END
 if defined _OLD_CODEPAGE (
