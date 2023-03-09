ad237e1a86537d7221ee30012a49e97122b22176
eacorsab <emascord@espol.edu.ec>
2023-03-09 12:12:55 -0500

    obtener por codigo

---
+++ api-Backend/app/productos/rutas/obtener_items.py
@@ -0,0 +1,30 @@
+from urllib import request
+from flask import jsonify
+from flask_cors import cross_origin
+from app.siacopc import bp
+from app.productos import bp
+
+from app.extensions import db
+from app.models.siacopc import Siacopc
+
+@bp.route('/obtener_items', methods=['GET'])
+@cross_origin()
+def obtener_items():
+    print("hola")
+    query_result = db.session.query(
+        Siacopc.modcodigo.label('modcodigo'),
+        Siacopc.opccaption.label('opccaption'),
+        Siacopc.opcname.label('opcname'),
+        Siacopc.opctag.label('opctag'),
+        Siacopc.opcmenujquery.label('opcmenuquery'),
+        Siacopc.nivel.label('nivel'),
+        Siacopc.item_number.label('item_number'),
+        Siacopc.padre_id.label('padre_id'),
+   )#.order_by(Producto.artcodigo).all()
+
+    # El resultado de la consulta es un objeto Row, que no puede ser serializado en JSON directamente.
+    # Para convertir los objetos Row en diccionarios se utiliza el método as_dict() que se puede agregar en el modelo de SQLAlchemy. 
+    # Luego, se puede construir una lista de diccionarios y devolverla como una respuesta JSON
+    data = [row._asdict() for row in query_result]
+    
+    return jsonify(data)
